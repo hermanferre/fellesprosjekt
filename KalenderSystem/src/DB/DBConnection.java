d5e0be0d043dde0cd91e44713000cfe77e4e1ad8
hallvarh <hallvarh@stud.ntnu.no>
2014-03-10 12:42:12 +0100

    Sql

--- KalenderSystem/src/DB/DBConnection.java
+++ KalenderSystem/src/DB/DBConnection.java
@@ -1,108 +1,65 @@
 package DB;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
 
 
 public class DBConnection {
-
-	private Properties properties; // file containing the connection properties
-	private String jdbcDriver; // String containing the driver Class name
-	private String url; // Address to the database
-	private Connection conn;
-
 	
-	public DBConnection(String propertiesFilename) throws IOException, ClassNotFoundException, SQLException {
-		File f = new File(propertiesFilename);
-		properties = new Properties();
-		properties.load(new FileInputStream(f));
-		jdbcDriver = properties.getProperty("jdbcDriver");
-		url = properties.getProperty("url");
+	private static Connection connection = null;
+	private static String connectionURL = "jdbc:mysql://mysql.stud.ntnu.no:3306/hallvarh_fp";
+	private static Statement stat = null;
+	private static String user = "hallvarh";
+	private static String pw = "gruppe42";
+	private static String database = "hallvarh";
+	
+	//Logger på databasen
+	public DBConnection(){
+		try{
+			connection = DriverManager.getConnection(connectionURL, user, pw);
+			stat = connection.createStatement();
+		} catch(Exception e){
+			System.out.println("Tilkobling til databaseserver feilet: " + e.getMessage());
+			System.out.println("Må være på NTNU-nettet for å koble til");
+		}
 	}
 	
-	
-	public DBConnection(Properties properties) throws ClassNotFoundException, SQLException {
-		jdbcDriver = properties.getProperty("jdbcDriver");
-		url = properties.getProperty("url");
-		this.properties=properties;
+	//Stenger tilkoblingen
+	public void close(){
+		try{
+			connection.close();
+		} catch(SQLException e){
+			throw new RuntimeException(e);
+		}
 	}
 	
-	
-	public void initialize() throws ClassNotFoundException, SQLException {
-		
-		Class.forName(jdbcDriver);
-		Properties info = new Properties();
-	
-		info.setProperty("user", properties.getProperty("user"));
-		info.setProperty("password", properties.getProperty("user"));
-		conn = DriverManager.getConnection(url, info);
-	}
-	
-	
-	public ResultSet makeSingleQuery(String sql) throws SQLException {
-		Statement st = conn.createStatement();
-		return st.executeQuery(sql);
-	}
-	
-	
-	public void makeSingleUpdate(String sql) throws SQLException {
-		Statement st = conn.createStatement();
-		st.executeUpdate(sql);
-	}
-	
-	
-	public PreparedStatement preparedStatement(String sql) throws SQLException {
-		return conn.prepareStatement(sql);
-	}
-	
-	
-	public void close() throws SQLException {
-		conn.close();
-	}
-
-}
-
-
-package util;
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-//Brukes for å koble applikasjonen opp mot serveren
-//Returnerer en connection man kan bruke for å lage MySQL statements(spørringer) og andre ting
-//ConnectionConfiguration configuration = new ConnectionConfiguration(); --> Skriv configuration. gir forskjellige muligheter for å 
-//gjøre en spørring
-
-public class ConnectionConfiguration {
-
-	private static String driver = "com.mysql.jdbc.Driver";
-	
-	// Database connection settings
-	private static String dbConnectionString = "jdbc:mysql://mysql.stud.ntnu.no:3306/martinoy_felles";
-	private static final String brukernavn = "martinoy";
-	private static final String passord = "nisse123";
-	
-	
-	// Returns a connection to database specified in connection settings
-	public static Connection getConnection(){
-		Connection con = null;
-			try {
-				Class.forName(driver);
-				con = DriverManager.getConnection(dbConnectionString, brukernavn , passord);
-				
-			} catch (Exception e) {
-				System.out.println("Could not establish connection to server. Please check your internet connection and connection settings. \n" + e.toString());
-			}
+	public ResultSet readQuery(String sql){
+		Statement s;
+		try{
+			s = connection.createStatement();
+			ResultSet rs;
 			
-		return con;
+			rs = s.executeQuery(sql);
+			return rs;
+		} catch (SQLException e){
+			throw new RuntimeException(e);
+		}
 	}
-
-
+	
+	public void updateQuery(String sql){
+		Statement s;
+		try{
+			s = connection.createStatement();
+			s.executeUpdate(sql);
+		} catch (SQLException e){
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public Connection getConnection() {
+		return connection;
+	}
 }
--- KalenderSystem/src/DB/Database.java
+++ KalenderSystem/src/DB/Database.java
@@ -1,25 +1,118 @@
 package DB;
 
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class Database {
 	
 	DBConnection db;
 	/**
 	 * @param args
+	 * @throws SQLException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws SQLException {
+		Database databaseTest = new Database();
+		System.out.println(databaseTest.getUsername());
+//		databaseTest.addPerson("h", "pw");
+//		databaseTest.addPerson("h", "pw");
+//		databaseTest.getUsername();
+//		databaseTest.removePerson("h");
+//		databaseTest.getUsername();
+//		databaseTest.getRooms();
+//		System.out.println(databaseTest.getRooms());
+//		databaseTest.addRoom(1, 2);
 	}
 	
-	public void getUsername(String username){
-		String query;
+	public Database(){
+		db = new DBConnection();
 	}
 	
-	public void getParrword(String username){
-		String query;
+	public ArrayList<String> getUsername(){
+		ArrayList<String> brukernavn = new ArrayList<String>();
+		String query = "select brukernavn from Ansatt;";
+		ResultSet rs = db.readQuery(query);
+		try{
+			while(rs.next()){
+				brukernavn.add(rs.getString("brukernavn"));
+			}
+		} catch(SQLException e){
+			throw new RuntimeException(e);
+		}
+		return brukernavn;
 	}
+	
+	public void addPerson(String user, String pw) throws SQLException{
+		ArrayList<String>brukernavn = getUsername();
+		if(brukernavn.contains(user)){
+			System.out.println("Brukeren finnes allerede, velg et annet brukernavn!");
+		} else{
+			String query = "insert into Ansatt values ('" + user + "', '" + pw + "');";
+			db.updateQuery(query);
+		}
+	}
+	
+	public void removePerson(String user){
+		String query = "delete from Ansatt where brukernavn = '" + user + "';";
+		db.updateQuery(query);
+	}
+	
+	public String getPassword(String username){
+		String query = "select passord from Ansatt where brukernavn = '" + username + "';";
+		ResultSet rs = db.readQuery(query);
+		String pw = null;
+		try {
+			if(rs.next()){
+				pw = rs.getString("passord");
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException();
+		}
+		System.out.println(pw);
+		return pw;
+	}
+	
+	
+	
+	public int getRoomCap(int roomNr){
+		String query = "select * from Moterom where romnr = '" + roomNr + "';";
+		ResultSet rs = db.readQuery(query);
+		int cap = 0;
+		try{
+			if(rs.next()){
+				cap = rs.getInt("kapasitet");
+			}
+		} catch(SQLException e){
+			throw new RuntimeException();
+		}
+		return cap;
+	}
+	
+	public ArrayList<Integer> getRooms(){
+		ArrayList<Integer> rooms = new ArrayList<Integer>();
+		String query = "select romnr from Moterom;";
+		ResultSet rs = db.readQuery(query);
+		try{
+			while(rs.next()){
+				int room = rs.getInt("romnr");
+				rooms.add(room);
+			}
+		} catch(SQLException e){
+			throw new RuntimeException(e);
+		}
+		return rooms;
+	}
+	
+	public void addRoom(int roomNr, int cap) throws SQLException{
+		ArrayList<Integer> rooms = getRooms();
+		if(rooms.contains(roomNr)){
+			System.out.println("Rommet finnes allerede, velg et annet brukernavn!");
+		} else{
+			String query = "insert into Moterom values (" + roomNr + ", " + cap + ");";
+			db.updateQuery(query);
+		}
+	}
+	
+	
 	
 	public void getStartEndTime(int avtaleID){
 		String query;
@@ -27,12 +120,6 @@ public class Database {
 	
 	public void getMeetingRoom(int avtaleID){
 		String query;
-	}
-	
-	public void getRoom(int roomNr) throws ClassNotFoundException, SQLException{
-		String query = String.format("select * from moterom where romnr = %d", roomNr);
-		db.initialize();
-		
 	}
 
 }
