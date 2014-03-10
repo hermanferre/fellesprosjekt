6b6ffdaadbc1f9494d696a870fc6b29442c7bfe5
hermanferre <herman@ferre.no>
2014-03-10 14:35:18 +0100

    + Kalendersystem.java

---
+++ KalenderSystem/src/Main/KalenderSystem.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class KalenderSystem {
+	
+	/**
+	 * Jeg driver på med denne nå! - Herman
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
