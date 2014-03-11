6ca4040fdf67eade77f5a95485d88ac95e2e350f
hallvarh <hallvarh@stud.ntnu.no>
2014-03-11 12:58:54 +0100

    Hei

--- KalenderSystem/src/DB/DBAvtale.java
+++ KalenderSystem/src/DB/DBAvtale.java
@@ -1,108 +1,19 @@
 package DB;
 
-import java.sql.*;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Time;
 import java.util.ArrayList;
 
-public class Database {
-	
+public class DBAvtale {
 	DBConnection db;
-	/**
-	 * @param args
-	 * @throws SQLException 
-	 */
-	public static void main(String[] args) throws SQLException {
-		Database databaseTest = new Database();
-
+	public static void main(String[] args) throws SQLException{
+		DBMoterom dbm = new DBMoterom();
 	}
-	
-	public Database(){
+	public DBAvtale(){
 		db = new DBConnection();
 	}
-	
-	public ArrayList<String> getUsername(){
-		ArrayList<String> brukernavn = new ArrayList<String>();
-		String query = "select brukernavn from Ansatt;";
-		ResultSet rs = db.readQuery(query);
-		try{
-			while(rs.next()){
-				brukernavn.add(rs.getString("brukernavn"));
-			}
-		} catch(SQLException e){
-			throw new RuntimeException(e);
-		}
-		return brukernavn;
-	}
-	
-	public void addPerson(String user, String pw) throws SQLException{
-		ArrayList<String>brukernavn = getUsername();
-		if(brukernavn.contains(user)){
-			System.out.println("Brukeren finnes allerede, velg et annet brukernavn!");
-		} else{
-			String query = "insert into Ansatt values ('" + user + "', '" + pw + "');";
-			db.updateQuery(query);
-		}
-	}
-	
-	public void removePerson(String user){
-		String query = "delete from Ansatt where brukernavn = '" + user + "';";
-		db.updateQuery(query);
-	}
-	
-	public String getPassword(String username){
-		String query = "select passord from Ansatt where brukernavn = '" + username + "';";
-		ResultSet rs = db.readQuery(query);
-		String pw = null;
-		try {
-			if(rs.next()){
-				pw = rs.getString("passord");
-			}
-		} catch (SQLException e) {
-			throw new RuntimeException();
-		}
-		return pw;
-	}
-	
-	
-	
-	public int getRoomCap(int roomNr){
-		String query = "select * from Moterom where romnr = '" + roomNr + "';";
-		ResultSet rs = db.readQuery(query);
-		int cap = 0;
-		try{
-			if(rs.next()){
-				cap = rs.getInt("kapasitet");
-			}
-		} catch(SQLException e){
-			throw new RuntimeException();
-		}
-		return cap;
-	}
-	
-	public ArrayList<Integer> getRooms(){
-		ArrayList<Integer> rooms = new ArrayList<Integer>();
-		String query = "select romnr from Moterom;";
-		ResultSet rs = db.readQuery(query);
-		try{
-			while(rs.next()){
-				int room = rs.getInt("romnr");
-				rooms.add(room);
-			}
-		} catch(SQLException e){
-			throw new RuntimeException(e);
-		}
-		return rooms;
-	}
-	
-	public void addRoom(int roomNr, int cap) throws SQLException{
-		ArrayList<Integer> rooms = getRooms();
-		if(rooms.contains(roomNr)){
-			System.out.println("Rommet finnes allerede, velg et annet brukernavn!");
-		} else{
-			String query = "insert into Moterom values (" + roomNr + ", " + cap + ");";
-			db.updateQuery(query);
-		}
-	}
-	
 	
 	public String getDate(int avtaleID){
 		String query = "select dato from Avtale where avtaleid = " + avtaleID + ";";
