893b3354e621f405b5a7d564097ba10b25177278
hallvarh <hallvarh@stud.ntnu.no>
2014-03-11 12:59:54 +0100

    gds

--- KalenderSystem/src/Main/EditAvtale.java
+++ KalenderSystem/src/Main/EditAvtale.java
@@ -1,5 +1,83 @@
 package Main;
 
-public class EditAvtale {
+import java.util.Scanner;
 
+public class EditAvtale {
+	public void editMeny(){
+		System.out.println("Angi avtaleid)");
+		Scanner sc = new Scanner(System.in);
+		String id = sc.nextLine();
+		int tall;
+		try {
+			tall = Integer.parseInt(id);
+		} catch (NumberFormatException e) {
+			System.out.println("Dette var ikke et tall. Føkk deg");
+		}
+		
+		System.out.println("0: endre starttid");
+		System.out.println("1: endre sluttid");
+		System.out.println("2: endre dato");
+		System.out.println("3: endre sted");
+		System.out.println("4: endre beskrivelse");
+		System.out.println("5: endre moterom");
+		Scanner sc = new Scanner(System.in);
+		String com = sc.nextLine();
+		
+		if(com == "0"){
+			editStart(tall);
+			else if(com == "1")
+				editSlutt(tall);
+			else if(com == "2")
+				editDato(tall);
+			else if(com =="3")
+				editSted(tall);
+			else if(com == "4")
+				editBeskrivelse(tall);
+			else if(com =="5")
+				editMoterom(tall);
+			else
+				System.out.println("ikke gyldig tall");	
+		}
+	public void editStart(int ID){
+		System.out.println("Legg til ny starttid(HH:MM:SS): ");
+		Scanner sc = new Scanner(System.in);
+		String com = sc.nextLine();
+		database.editStart(ID, com);
+	}
+	public void editSlutt(int ID){
+		System.out.println("Legg til ny sluttid(HH:MM:SS): ");
+		Scanner sc = new Scanner(System.in);
+		String com = sc.nextLine();
+		database.editEnd(ID, com);
+	}
+	public void editDato(int ID){
+		System.out.println("Legg til ny dato(YYYY-MM-DD): ");
+		Scanner sc = new Scanner(System.in);
+		String com = sc.nextLine();
+		database.editDate(ID, com);
+	}
+	public void editSted(int ID){
+		System.out.println("Legg til ny stedsbeskrivelse: ");
+		Scanner sc = new Scanner(System.in);
+		String com = sc.nextLine();
+		database.editPlace(ID, com);
+	}
+	public void editBeskrivelse(int ID){
+		System.out.println("Legg til ny stedsbeskrivelse: ");
+		Scanner sc = new Scanner(System.in);
+		String com = sc.nextLine();
+		database.editBeskrivelse(tall, com)
+	}
+	public void editMoterom(int ID){
+		System.out.println("Legg til ny stedsbeskrivelse: ");
+		Scanner sc = new Scanner(System.in);
+		String com = sc.nextLine();
+		int tall2;
+		try {
+			int tall2 = Integer.parseInt(com);
+		} catch (NumberFormatException e) {
+			System.out.println("Dette var ikke et tall. Føkk deg");
+		}
+		database.editMeetingroom(ID, tall2)
+	}
 }
