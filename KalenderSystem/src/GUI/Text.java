fb4e004044927963dd7414f8662eaaf0a4901b12
hermanferre <herman@ferre.no>
2014-03-11 11:51:39 +0100

    Flytter teksten til en annen fil i GUI-mappen

---
+++ KalenderSystem/src/GUI/Text.java
@@ -0,0 +1,19 @@
+package GUI;
+
+public class Text {
+
+	public static String logginn = 		"Vennligst logg inn";
+	public static String brukernavn = 	"Brukernavn:";
+	public static String passord = 		"Passord:";
+	public static String logginn_ok = 	"Innlogging OK!";
+	public static String logginn_feil = "Innlogging feilet, pr¿v igjen!";
+	public static String velkommen = 	"Velkommen til kalendersystemet!\nHva ¿nsker du Œ gj¿re idag (skriv tall)?";
+	public static String[] valg = 		{"Avslutt",
+										"Legg til avtale",
+										"Endre avtale",
+										"Legg til ansatt",
+										"Endre ansatt",
+										"Legg til avtale",
+										"Endre avtale"};
+	public static String avsluttet =	"Du har avsluttet programmet. Ha en fortsatt fin dag.";
+}
