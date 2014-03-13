8ab11aaf7e067a4faef78f48947befde81d11461
havstad <oystein.havstad@gmail.com>
2014-03-13 12:12:16 +0100

    begynt på buildweek

--- KalenderSystem/src/GUI/BuildWeek.java
+++ KalenderSystem/src/GUI/BuildWeek.java
@@ -1,12 +1,143 @@
 package GUI;
 
-public class BuildWeek {
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import DB.Database;
+import Main.Appointment;
+
+public class BuildWeek extends JDialog implements ActionListener {
+	
+	private JButton nesteukeknapp, forjeukeknapp, lukkeknapp;
+	private JLabel labelukenummer;
+	private JScrollPane skrolleomrade;
+	
+	private String[] kolonner = {"Mandag","Tirsdag", "Onsdag", "Torsdag", 
+			"Fredag", "Lørdag", "Søndag"};
+	
+	private JLabel[][] tabell;
+	private JPanel tabellbeholder;
+	
+	private int ukenummer;
+	
+	public BuildWeek() {
+		setTitle("ukeoversikt");
+		
+		Calendar cal = Calendar.getInstance();
+		ukenummer = cal.get(Calendar.WEEK_OF_YEAR);
+		
+		Container cont = getContentPane();
+		
+		JPanel verktøylinje = new JPanel();
+		nesteukeknapp = new JButton("->");
+		forjeukeknapp = new JButton("<-");
+		nesteukeknapp.addActionListener(this);
+		forjeukeknapp.addActionListener(this);
+		
+		labelukenummer = new JLabel(""+ukenummer);
+		
+		verktøylinje.add(forjeukeknapp);
+		verktøylinje.add(labelukenummer);
+		verktøylinje.add(nesteukeknapp);
+		
+		
+		lukkeknapp = new JButton("Lukk");
+		lukkeknapp.addActionListener(this);
+		
+		skrolleomrade = new JScrollPane();
+		cont.add(skrolleomrade, BorderLayout.CENTER);
+		
+		
+		
+		cont.add(verktøylinje, BorderLayout.NORTH);
+		cont.add(lukkeknapp, BorderLayout.SOUTH);
+		pack();
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource(); 
+		
+		if(source == lukkeknapp) {
+			setVisible(false);
+			dispose();
+		} else if(source == nesteukeknapp) {
+			ukenummer++;
+			oppdaterukevisning();
+		} else if(source == forjeukeknapp) {
+			ukenummer--;
+			oppdaterukevisning();
+			
+		}
+		
+	}
+	
+	public void oppdaterukevisning() {
+		labelukenummer.setText(""+ukenummer);
+	}
+	
+	public static void buidlWeek(String brukernavn, int ukenummer) {
+		Database db = new Database();
+		
+		
+		ArrayList<Appointment> avtaleliste = db.getAppointments(brukernavn);
+		
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.WEEK_OF_YEAR, ukenummer);
+		
+		
+		//set kalenderobjektet til mandag
+		cal.set(Calendar.DAY_OF_WEEK, cal.getFirstDayOfWeek());
+		
+		
+		//løkk igjennom uka
+		for( int j = 0; j < 7; j++) {
+			//System.out.println(dateFormat.format(cal.getTime()));
+			
+			
+			for(int i = 0; i < avtaleliste.size(); i++) {
+				//System.out.println(avtaleliste.get(i).dato);
+				//System.out.println(dateFormat.format(cal.getTime()));
+				
+				Appointment avtale = avtaleliste.get(i);
+				
+				if(avtale.dato.equals(dateFormat.format(cal.getTime()))) {
+					System.out.print(avtale.description);
+				}
+			}
+			
+			cal.add(Calendar.DATE, 1);
+
+		}
+		
+
+		
+		
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//buidlWeek("Hallvard", 11);
+		
+		BuildWeek buildweek = new BuildWeek();
+		buildweek.setVisible(true);
 
 	}
 
