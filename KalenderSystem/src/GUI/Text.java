0306937591705c941268ff01d3ae28833476f5a7
hermanferre <herman@ferre.no>
2014-03-14 13:36:57 +0100

    Mer menyfunksjonalitet

--- KalenderSystem/src/GUI/Text.java
+++ KalenderSystem/src/GUI/Text.java
@@ -3,12 +3,24 @@ package GUI;
 public class Text {
 
 	// KALENDERSYSTEM
-	public static String logginn = 		"Vennligst logg inn";
+	public static String[] brukervalg =	{"Avslutt",
+										"Logg inn",
+										"Opprett ny bruker"};
+	//public static String opprettbruker= "Opprett ny bruker";
+	public static String opprett_ok =	"Du har opprettet brukeren: ";
+	public static String opprett_feilp=	"Oppretting feilet! Passordene var ikke like.";
+	public static String opprett_feilb= "Oppretting feilet! Brukernavnet eksisterer allerede";
+	public static String returneres =	"Du returneres til forrige meny.";
+	
+	//public static String logginn = 		"Logg inn";
 	public static String brukernavn = 	"Brukernavn:";
 	public static String passord = 		"Passord:";
+	public static String gjenta_pw =	"Gjenta passord:";
+	public static String email =		"E-post-adresse:";
 	public static String logginn_ok = 	"Innlogging OK!";
 	public static String logginn_feil = "Innlogging feilet, prov igjen!";
-	public static String velkommen = 	"Velkommen til kalendersystemet!\nHva onsker du a gjore idag (skriv tall)?";
+	public static String velkommen = 	"Velkommen til kalendersystemet!";
+	public static String valg =			"Her er dine valg.\nHva onsker du a gjore idag (skriv tall)?";
 	public static String[] hovedvalg = 	{"Avslutt",
 										"Min kalender",
 										"Avtaler",
--- KalenderSystem/src/Main/KalenderSystem.java
+++ KalenderSystem/src/Main/KalenderSystem.java
@@ -31,31 +31,28 @@ public class KalenderSystem {
 
 	public static void main(String[] args) {
 		KalenderSystem ks = new KalenderSystem();
+		ks.startMenu();
 		ks.run();
 	}
 
-	public void run() {
+	public void startMenu() {
+		System.out.println(Text.velkommen);
 		while (!loggedin) {
-			String[] data = new String[2];
-			System.out.println(Text.logginn);
-			System.out.println(Text.brukernavn);
-			data[0] = scanner.next();
-			System.out.println(Text.passord);
-			data[1] = scanner.next();
-
-			if (data[1].equals(db.getPassword(data[0]))) {
-				System.out.println(Text.logginn_ok);
-				loggedin = true;
-				user = data[0];
-			} else {
-				System.out.println(Text.logginn_feil);
-				//System.out.println("Passord skulle v¾rt \""+db.getPassword(data[0])+"\", men var \""+data[1]+"\"");
+			switch (printMenu(Text.brukervalg)) {
+			case 0:
+				System.out.println(Text.avsluttet);
+				run = false;
+				break;
+			case 1: login(); break;
+			case 2: addUser(); break;
 			}
 		}
+	}
 
+	public void run() {
 		while (run) {
 
-			System.out.println(Text.velkommen);
+			System.out.println(Text.valg);
 
 			switch (printMenu(Text.hovedvalg)) {
 			case 0:
@@ -81,16 +78,64 @@ public class KalenderSystem {
 		}
 	}
 
+
+	public void login() {
+		while (!loggedin) {
+			String[] data = new String[2];
+			System.out.println(Text.brukervalg[1]);
+			System.out.println(Text.brukernavn);
+			data[0] = scanner.next();
+			System.out.println(Text.passord);
+			data[1] = scanner.next();
+
+			if (data[1].equals(db.getPassword(data[0]))) {
+				System.out.println(Text.logginn_ok);
+				loggedin = true;
+				user = data[0];
+			} else {
+				System.out.println(Text.logginn_feil);
+				//System.out.println("Passord skulle v¾rt \""+db.getPassword(data[0])+"\", men var \""+data[1]+"\"");
+			}
+		}
+	}
+
+	public void addUser() {
+		String[] data = new String[4];
+		System.out.println(Text.brukervalg[2]);
+		System.out.println(Text.brukernavn);
+		data[0] = scanner.next();
+		if (db.getPassword(data[0]) != null) {
+			System.out.println(Text.opprett_feilb);
+			System.out.println(Text.returneres);
+		} else {
+			System.out.println(Text.passord);
+			data[1] = scanner.next();
+			System.out.println(Text.gjenta_pw);
+			data[2] = scanner.next();
+			if (!data[1].equals(data[2])) {
+				System.out.println(Text.opprett_feilp);
+				System.out.println(Text.returneres);
+			} else {
+				System.out.println(Text.email);
+				data[3] = scanner.next();
+				loggedin = true;
+				user = data[0];
+				System.out.println(Text.opprett_ok + user);
+				db.addPerson(data[0], data[1], data[3]);
+			}
+		}
+	}
+
 	private void viewCalendar() {
 		switch(printMenu(Text.kalvalg)) {
 		case 0: break;
 		case 1: showCalendar(getUser()); break;
-		case 2: break;
+		case 2: a.statusMeny(); break;
 		case 3: 
 			System.out.println(Text.hvilkenbruker);
 			showCalendar(scanner.next()); break;
 		}
-		
+
 	}
 
 	private void showCalendar(String user) {
@@ -112,15 +157,15 @@ public class KalenderSystem {
 		case 1: Ansatt.addAnsatt(); break;
 		case 2: Ansatt.editAnsatt(); break;
 		}
-		
+
 	}
 
 	private void meetingRoomMenu() {
 		switch(printMenu(Text.romvalg)) {
 		case 0: break;
-		
+
 		}
-		
+
 	}
 
 	public int printMenu(String[] valg) {
@@ -129,7 +174,7 @@ public class KalenderSystem {
 		}
 		return scanner.nextInt();
 	}
-	
+
 	public static String getUser() {
 		return user;
 	}
